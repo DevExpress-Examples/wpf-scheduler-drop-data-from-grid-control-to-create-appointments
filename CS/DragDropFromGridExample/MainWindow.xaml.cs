09b9226e1eab12776b64049ae2600f3b506acc53
dxIvan <38530056+ivansupport@users.noreply.github.com>
2022-01-20 12:13:29 +0300

    fix drag and drop

--- CS/DragDropFromGridExample/MainWindow.xaml.cs
+++ CS/DragDropFromGridExample/MainWindow.xaml.cs
@@ -1,56 +1,22 @@
-﻿using DevExpress.Xpf.Scheduling;
-using System;
-using System.Collections.Generic;
+﻿using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Scheduling;
 using System.Linq;
-using System.Windows;
-using System.Windows.Input;
 
 namespace DragDropFromGridExample {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : DevExpress.Xpf.Core.ThemedWindow {
-        private bool startDrag;
-        private Point startPoint;
-
+    public partial class MainWindow : ThemedWindow {
         public MainWindow() {
             InitializeComponent();
-            grdPatients.View.PreviewMouseDown += View_PreviewMouseDown;
-            grdPatients.View.PreviewMouseMove += View_PreviewMouseMove;
         }
-
-        DataObject GetSchedulerData(int rowHandler) {
-            List<AppointmentItem> appointments = new List<AppointmentItem>();
-            Patient patient = grdPatients.GetRow(rowHandler) as Patient;
-            AppointmentItem appointment = new AppointmentItem();
-            appointment.Subject = patient.Name;
-            appointment.LabelId = scheduler.LabelItems[0].Id;
-            appointment.StatusId = scheduler.StatusItems[1].Id;
-            appointments.Add(appointment);
-            return new DataObject(typeof(IEnumerable<AppointmentItem>), appointments);
-        }
-
-        bool IsGridRowAvailable(int rowHandler) {
-            return grdPatients.GetRow(rowHandler) != null;
-        }
-
-        void View_PreviewMouseDown(object sender, MouseButtonEventArgs e) {
-            if (e.LeftButton != MouseButtonState.Pressed)
-                return;
-            startPoint = e.GetPosition(null);
-            startDrag = IsGridRowAvailable(grdPatients.View.GetRowHandleByMouseEventArgs(e));
-        }
-
-        void View_PreviewMouseMove(object sender, MouseEventArgs e) {
-            Point position = e.GetPosition(null);
-            if (startDrag && e.LeftButton == MouseButtonState.Pressed) {
-                int rowHandler = grdPatients.View.GetRowHandleByMouseEventArgs(e);
-                if (IsGridRowAvailable(rowHandler)) {
-                    startDrag = false;
-                    DragDropEffects de = DragDrop.DoDragDrop(grdPatients, GetSchedulerData(rowHandler), DragDropEffects.All);
-                }
+        private void scheduler_StartAppointmentDragFromOutside(object sender, StartAppointmentDragFromOutsideEventArgs e) {
+            if (e.Data.GetDataPresent(typeof(RecordDragDropData))) {
+                var data = (RecordDragDropData)e.Data.GetData(typeof(RecordDragDropData));
+                foreach (var x in data.Records.OfType<Patient>())
+                    e.DragAppointments.Add(new AppointmentItem {
+                        Subject = x.Name,
+                        LabelId = scheduler.LabelItems.FirstOrDefault()?.Id,
+                        StatusId = scheduler.StatusItems.FirstOrDefault()?.Id
+                    });
             }
-            this.startPoint = position;
         }
     }
 }
--- VB/DragDropFromGridExample/MainWindow.xaml.vb
+++ VB/DragDropFromGridExample/MainWindow.xaml.vb
@@ -1,60 +1,25 @@
-﻿Imports DevExpress.Xpf.Scheduling
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Windows
-Imports System.Windows.Input
+﻿Imports DevExpress.Xpf.Core
+Imports DevExpress.Xpf.Scheduling
 
 Namespace DragDropFromGridExample
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
     Partial Public Class MainWindow
-        Inherits DevExpress.Xpf.Core.ThemedWindow
-
-        Private startDrag As Boolean
-        Private startPoint As Point
+        Inherits ThemedWindow
 
         Public Sub New()
             InitializeComponent()
-            AddHandler grdPatients.View.PreviewMouseDown, AddressOf View_PreviewMouseDown
-            AddHandler grdPatients.View.PreviewMouseMove, AddressOf View_PreviewMouseMove
         End Sub
 
-        Private Function GetSchedulerData(ByVal rowHandler As Integer) As DataObject
-            Dim appointments As New List(Of AppointmentItem)()
-
-            Dim patient_Renamed As Patient = TryCast(grdPatients.GetRow(rowHandler), Patient)
-            Dim appointment As New AppointmentItem()
-            appointment.Subject = patient_Renamed.Name
-            appointment.LabelId = scheduler.LabelItems(0).Id
-            appointment.StatusId = scheduler.StatusItems(1).Id
-            appointments.Add(appointment)
-            Return New DataObject(GetType(IEnumerable(Of AppointmentItem)), appointments)
-        End Function
-
-        Private Function IsGridRowAvailable(ByVal rowHandler As Integer) As Boolean
-            Return grdPatients.GetRow(rowHandler) IsNot Nothing
-        End Function
-
-        Private Sub View_PreviewMouseDown(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
-            If e.LeftButton <> MouseButtonState.Pressed Then
-                Return
+        Private Sub scheduler_StartAppointmentDragFromOutside(sender As Object, e As StartAppointmentDragFromOutsideEventArgs)
+            If e.Data.GetDataPresent(GetType(RecordDragDropData)) Then
+                Dim Data = CType(e.Data.GetData(GetType(RecordDragDropData)), RecordDragDropData)
+                For Each x In Data.Records.OfType(Of Patient)
+                    e.DragAppointments.Add(New AppointmentItem() With {
+                        .Subject = x.Name,
+                        .LabelId = scheduler.LabelItems.FirstOrDefault()?.Id,
+                        .StatusId = scheduler.StatusItems.FirstOrDefault()?.Id
+                    })
+                Next
             End If
-            startPoint = e.GetPosition(Nothing)
-            startDrag = IsGridRowAvailable(grdPatients.View.GetRowHandleByMouseEventArgs(e))
-        End Sub
-
-        Private Sub View_PreviewMouseMove(ByVal sender As Object, ByVal e As MouseEventArgs)
-            Dim position As Point = e.GetPosition(Nothing)
-            If startDrag AndAlso e.LeftButton = MouseButtonState.Pressed Then
-                Dim rowHandler As Integer = grdPatients.View.GetRowHandleByMouseEventArgs(e)
-                If IsGridRowAvailable(rowHandler) Then
-                    startDrag = False
-                    Dim de As DragDropEffects = DragDrop.DoDragDrop(grdPatients, GetSchedulerData(rowHandler), DragDropEffects.All)
-                End If
-            End If
-            Me.startPoint = position
         End Sub
     End Class
 End Namespace
