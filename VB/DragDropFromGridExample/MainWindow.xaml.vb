8afb2ef813d30b776f045d56c39e80ae4405385d
DevExpressExampleBot <fakeEmail>
2023-11-22 13:55:18 +0400

    Source auto update [skip ci]

--- VB/DragDropFromGridExample/MainWindow.xaml.vb
+++ VB/DragDropFromGridExample/MainWindow.xaml.vb
@@ -1,60 +1,58 @@
-﻿Imports DevExpress.Xpf.Scheduling
-Imports System
+Imports DevExpress.Xpf.Scheduling
 Imports System.Collections.Generic
-Imports System.Linq
 Imports System.Windows
 Imports System.Windows.Input
 
 Namespace DragDropFromGridExample
+
     ''' <summary>
     ''' Interaction logic for MainWindow.xaml
     ''' </summary>
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits DevExpress.Xpf.Core.ThemedWindow
 
         Private startDrag As Boolean
+
         Private startPoint As Point
 
         Public Sub New()
-            InitializeComponent()
-            AddHandler grdPatients.View.PreviewMouseDown, AddressOf View_PreviewMouseDown
-            AddHandler grdPatients.View.PreviewMouseMove, AddressOf View_PreviewMouseMove
+            Me.InitializeComponent()
+            AddHandler Me.grdPatients.View.PreviewMouseDown, AddressOf Me.View_PreviewMouseDown
+            AddHandler Me.grdPatients.View.PreviewMouseMove, AddressOf Me.View_PreviewMouseMove
         End Sub
 
         Private Function GetSchedulerData(ByVal rowHandler As Integer) As DataObject
-            Dim appointments As New List(Of AppointmentItem)()
-
-            Dim patient_Renamed As Patient = TryCast(grdPatients.GetRow(rowHandler), Patient)
-            Dim appointment As New AppointmentItem()
-            appointment.Subject = patient_Renamed.Name
-            appointment.LabelKey = scheduler.LabelItems(0).Id
-            appointment.StatusKey = scheduler.StatusItems(1).Id
+            Dim appointments As List(Of AppointmentItem) = New List(Of AppointmentItem)()
+            Dim patient As Patient = TryCast(Me.grdPatients.GetRow(rowHandler), Patient)
+            Dim appointment As AppointmentItem = New AppointmentItem()
+            appointment.Subject = patient.Name
+            appointment.LabelKey = Me.scheduler.LabelItems(0).Id
+            appointment.StatusKey = Me.scheduler.StatusItems(1).Id
             appointments.Add(appointment)
             Return New DataObject(GetType(IEnumerable(Of AppointmentItem)), appointments)
         End Function
 
         Private Function IsGridRowAvailable(ByVal rowHandler As Integer) As Boolean
-            Return grdPatients.GetRow(rowHandler) IsNot Nothing
+            Return Me.grdPatients.GetRow(rowHandler) IsNot Nothing
         End Function
 
         Private Sub View_PreviewMouseDown(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
-            If e.LeftButton <> MouseButtonState.Pressed Then
-                Return
-            End If
+            If e.LeftButton <> MouseButtonState.Pressed Then Return
             startPoint = e.GetPosition(Nothing)
-            startDrag = IsGridRowAvailable(grdPatients.View.GetRowHandleByMouseEventArgs(e))
+            startDrag = Me.IsGridRowAvailable(Me.grdPatients.View.GetRowHandleByMouseEventArgs(e))
         End Sub
 
         Private Sub View_PreviewMouseMove(ByVal sender As Object, ByVal e As MouseEventArgs)
             Dim position As Point = e.GetPosition(Nothing)
             If startDrag AndAlso e.LeftButton = MouseButtonState.Pressed Then
-                Dim rowHandler As Integer = grdPatients.View.GetRowHandleByMouseEventArgs(e)
+                Dim rowHandler As Integer = Me.grdPatients.View.GetRowHandleByMouseEventArgs(e)
                 If IsGridRowAvailable(rowHandler) Then
                     startDrag = False
-                    Dim de As DragDropEffects = DragDrop.DoDragDrop(grdPatients, GetSchedulerData(rowHandler), DragDropEffects.All)
+                    Dim de As DragDropEffects = DragDrop.DoDragDrop(Me.grdPatients, GetSchedulerData(rowHandler), DragDropEffects.All)
                 End If
             End If
-            Me.startPoint = position
+
+            startPoint = position
         End Sub
     End Class
 End Namespace
